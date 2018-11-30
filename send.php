6bd56974df22d27960c1f820219f8e2b48ca6224
Sagar Poudel <poudelsagar530@gmail.com>
2018-11-29 23:15:49 -0600

    Update send.php

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ $msg = $_POST['msgText'];
 $sent_by = $_POST['sent_by'];
 
 try{
-$pdo = new PDO("mysql://b6f2b30ec08555:6c63d71f@us-cdbr-iron-east-01.cleardb.net/heroku_45f6e70ea9a7def?reconnect=true", 'b6f2b30ec08555', '6c63d71f');
+$pdo = new PDO("mysql:host=w29ifufy55ljjmzq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=xp2scq2gzxc9775i", 'co5ouhslws2gsya7', 'cfe57qnrmfee44ud');
 
 $sql_insert = "insert into message (msg, sent_by ) values ('$msg', '$sent_by')";
 
